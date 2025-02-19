a5b43afd4c978efd3eac97c62deca6f20f9f641a
manthan-vw <manthan.vijaywargia@darwinbox.io>
2025-02-19 11:40:31 +0530

    Initial Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-red-900 underline">Social Sphere! </h1>
+       </div>
+    </>
+  )
+}
+
+export default App
